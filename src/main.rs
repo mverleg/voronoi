dfdb17b2eccc33aa31d584f114b142fccc71baf0
Mark <mverleg.noreply@gmail.com>
2018-08-14 21:24:03 +0200

    Open and show image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![feature(extern_prelude)]
+
+use std::path::Path;
+use std::env;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let pth = Path::new("resources").join("imgs").join("parrots.png");
+    let img = image::open(pth).unwrap();
+//    println!("{:?}", img);
+    let mut outpth = env::temp_dir().join("voronoi_gen.png");
+    img.save(outpth.clone()).unwrap();
+    Command::new("eog").arg(outpth).spawn().unwrap();
 }
