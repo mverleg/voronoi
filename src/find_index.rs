14d1fd107c6a1a1484308a24d6e7e083e188ac3a
Mark <mverleg.noreply@gmail.com>
2018-08-19 22:15:47 +0200

    Make the bisection compile

--- src/find_index.rs
+++ src/find_index.rs
@@ -1,5 +1,6 @@
 use std::cmp::Ordering;
 use std::ops::{Add, Sub, Div};
+use std::ops::Range;
 
 /// Find an index that is equal in an ORDERED range.
 /// If there are multiple matches, there is no guarantee about which of them is returned.
@@ -7,18 +8,20 @@ use std::ops::{Add, Sub, Div};
 /// * if f(x) is Equal, f(x+1) is Equal or Greater
 /// * if f(x) is Greater, f(x+1) is Greater
 fn find_index<T, F>(mut min: T, mut max: T, f: F) -> Option<T>
-    where T: PartialOrd + Add<usize, Output=T> + Add<T, Output=T> + Sub<usize, Output=T> + Sub<T, Output=T> + Div<usize, Output=T>,
+    where T: PartialOrd + Add<usize, Output=T> + Add<T, Output=T> + Sub<usize, Output=T> + Sub<T, Output=T> + Div<usize, Output=T> + Copy,
           F: Fn(T) -> Ordering
 {
     assert!(max >= min);
     // Test the order criterion if in debug mode
-    for k in (min .. max - 1).iter() {
+    let mut k = min;
+    while k < max - 1 {
         if f(k) == Ordering::Equal {
             debug_assert!(f(k+1) == Ordering::Equal || f(k+1) == Ordering::Greater);
         }
         if f(k) == Ordering::Greater {
             debug_assert!(f(k+1) == Ordering::Greater);
         }
+        k = k + 1;
     }
     max = max + 1;
     // Bisection
