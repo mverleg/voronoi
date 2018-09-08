d2db0d3a2e2937820cb459fcec37e1eed30f0cbe
Mark <mverleg.noreply@gmail.com>
2018-09-08 22:42:00 +0200

    Create layout of paint code

---
+++ src/paint.rs
@@ -0,0 +1,26 @@
+use grouping::Grouping;
+
+/// Set the color of each pixel to the average of the group.
+pub fn pixel_to_group_colors(groups: Grouping) {
+    unimplemented!() // TODO: mark
+}
+
+/// Like [pixel_to_group_colors], but updates the image in-place.
+pub fn pixel_to_group_colors_inplace() {
+    unimplemented!() // TODO: mark
+}
+
+/// Create data structure to hold color averages for centers.
+pub fn make_center_colors() {
+    unimplemented!() // TODO: mark
+}
+
+/// Calculate the average color of the centers.
+pub fn calculate_center_colors() {
+    unimplemented!() // TODO: mark
+}
+
+/// Apply the center's average color to each pixel that belongs to it.
+pub fn paint_pixelsto_group_color() {
+    unimplemented!() // TODO: mark
+}
