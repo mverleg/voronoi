0dc00c35e10559481e8be8deb7709badd4bf26dd
Mark <mverleg.noreply@gmail.com>
2018-09-20 20:23:36 +0200

    Command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 #![feature(extern_prelude)]
 #![feature(nll)]
 
+extern crate clap;
 extern crate image;
 extern crate rand;
 extern crate threadpool;
 
 use assign::assign_to_centers;
-use img::Img;
+use clap::App;
+use clap::Arg;
 use distribute::generate_random_points;
 use grouping::Grouping;
+use img::Img;
 use paint::pixel_to_group_colors;
 use rand::{SeedableRng, StdRng};
 use std::env;
@@ -36,9 +39,46 @@ pub mod pointset;
 //TODO @mark: find a way to turn of all asserts in optimized mode? => or just convert the hot-loop-ones to debug_assert and keep the rest
 
 fn main() {
-    // Load an image
-    let pth = Path::new("resources").join("imgs").join("parrots.png");
-    let voronoi = voronoiify_image(Img::load(pth.as_path()));
+    let args = App::new("Voronoiify")
+        .version("1.0")
+        .about("Group image into voronoi-based patches and assign the average color to each patch")
+        .arg(Arg::with_name("input")
+            .help("Input png file to voronoiify")
+            .required(true)
+            .value_name("IN_PTH")
+            .index(1))
+        .arg(Arg::with_name("output")
+            .help("Path to store the generated file")
+            .short("o")
+            .long("output")
+            .value_name("OUT_PTH")
+            .takes_value(true))
+        .arg(Arg::with_name("count")
+            .help("Number of color patches to divide the image into")
+            .short("c")
+            .long("center_count")
+            .value_name("CENTERS")
+            .takes_value(true))
+        .arg(Arg::with_name("show")
+            .help("Show the generated image using EOG")
+            .short("s")
+            .long("show"))
+        .arg(Arg::with_name("seed")
+            .help("Use the given random seed")
+            .short("r")
+            .long("seed")
+            .value_name("SEED")
+            .takes_value(true))
+        .get_matches();
+
+    println!("{:?}", args);
+
+
+    let pth = Path::new(args.value_of("input").unwrap());
+    if !pth.exists() {
+        eprintln!("File {} does not exist", pth.display());
+    }
+    let voronoi = voronoiify_image(Img::load(pth));
     let outpth = env::temp_dir().join("voronoi_gen.png");
     voronoi.save(outpth.clone()).unwrap();
     Command::new("eog").arg(outpth).spawn().unwrap();
@@ -57,4 +97,15 @@ pub fn voronoiify_image(img: Img) -> Img {
     let groups = assign_to_centers(pixel_group, center_points);
     let voronoi = pixel_to_group_colors(groups, center_colors, img);
     voronoi
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+//    #[bench]
+//    fn test_full_flow_performance() {
+//        let pth = Path::new("resources").join("imgs").join("parrots.png");
+//        let voronoi = voronoiify_image(Img::load(pth.as_path()));
+//    }
 }
