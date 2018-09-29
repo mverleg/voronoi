59beab499348c1934098e0211fe751ee63984409
Mark <mverleg.noreply@gmail.com>
2018-09-29 22:43:25 +0200

    Threadpool works (with number of cpus)

--- isolated/threading/src/main.rs
+++ isolated/threading/src/main.rs
@@ -2,12 +2,33 @@ extern crate threadpool;
 
 use std::env;
 use threadpool::ThreadPool;
+use std::sync::mpsc::sync_channel;
 
 pub fn main() {
-    let pool = ThreadPool::new(5);
+
+    // Arguments
     let args: Vec<_> = env::args().collect();
-    let n = args[1].parse::<u64>().unwrap_or(45);
-    println!("fib #{} is {}", n, fib(n));
+    let n = args[1].parse::<u64>().unwrap_or(44);
+
+    // Pool
+    let pool = ThreadPool::default();
+    let (tx, rx) = sync_channel::<(u64, u64)>(3);
+
+    // Delegate work
+    for k in 0 .. 20 {
+        let t = tx.clone();
+        pool.execute(move || {
+            t.send((k, fib(n))).unwrap();
+        });
+    }
+    println!("work delegates, start reading");
+
+    // Read the output
+    for (k, fibk) in rx.iter().take(20) {
+        println!("fib #{} is {}", k, fibk);
+    }
+    pool.join();
+    println!("all done");
 }
 
 pub fn fib(n: u64) -> u64 {
