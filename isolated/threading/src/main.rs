64638ae9c90c0ebe889862683c5af091d2a88153
Mark <mverleg.noreply@gmail.com>
2018-10-01 21:58:38 +0200

    Make scoped pools work in MWE

--- isolated/threading/src/main.rs
+++ isolated/threading/src/main.rs
@@ -1,43 +1,48 @@
-extern crate threadpool;
+extern crate scoped_pool;
+extern crate num_cpus;
+
+use scoped_pool::Pool;
 
 use std::env;
-use threadpool::ThreadPool;
 use std::sync::mpsc::sync_channel;
 
 pub fn main() {
 
     // Arguments
     let args: Vec<_> = env::args().collect();
-    let n = if args.len() >= 2 {
+    let cnt = Box::new(if args.len() >= 2 {
         args[1].parse::<u64>().unwrap_or(44)
     } else {
         44
-    };
+    });
 
     // Pool
-    let pool = ThreadPool::default();
+    let pool = Pool::new(num_cpus::get());
     let (tx, rx) = sync_channel::<(u64, u64)>(3);
 
-    // Delegate work
-    for k in 0 .. 20 {
-        let t = tx.clone();
-        pool.execute(move || {
-            t.send((k, fib(n))).unwrap();
-        });
-    }
+    pool.scoped(|scope| {
+        // Delegate work
+        for k in 0 .. 20 {
+            let t = tx.clone();
+            let n = &cnt;
+            scope.execute(move ||
+                t.send((k, fib(&n))).unwrap()
+            );
+        }
+        // Read the output
+        for (k, fibk) in rx.iter().take(20) {
+            println!("fib #{} is {}", k, fibk);
+        }
+    });
     println!("work delegates, start reading");
 
-    // Read the output
-    for (k, fibk) in rx.iter().take(20) {
-        println!("fib #{} is {}", k, fibk);
-    }
-    pool.join();
+    pool.shutdown();
     println!("all done");
 }
 
-pub fn fib(n: u64) -> u64 {
-    if n <= 2 {
+pub fn fib(n: &u64) -> u64 {
+    if n <= &2 {
         return 1;
     }
-    fib(n - 1) + fib(n - 2)
+    fib(&(n - 1)) + fib(&(n - 2))
 }
